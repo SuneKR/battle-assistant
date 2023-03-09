834711280490d97c6e4b50094a61e145bec7ea40
SuneKR <SuneKRmail@gmail.com>
2023-03-09 10:59:45 +0100

    Renamed various pages and created a GridContainer

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import TabsPage from '../views/TabsPage.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/tabs/battlemap'
   },
   {
     path: '/tabs/',
@@ -13,18 +13,18 @@ const routes: Array<RouteRecordRaw> = [
     children: [
       {
         path: '',
-        redirect: '/tabs/tab1'
+        redirect: '/tabs/battlemap'
       },
       {
-        path: 'tab1',
+        path: 'battlemap',
         component: () => import('@/views/Tab1Page.vue')
       },
       {
-        path: 'tab2',
+        path: 'tokens',
         component: () => import('@/views/Tab2Page.vue')
       },
       {
-        path: 'tab3',
+        path: 'bugs',
         component: () => import('@/views/Tab3Page.vue')
       }
     ]
